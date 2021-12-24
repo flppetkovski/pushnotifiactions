54343aef1e555b4f8d1d293cffa5a40519ef0f78
Filip Petkovski <flp_petkovski@yahoo.com>
2021-12-24 09:47:21 +0100

    easy fix?

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 self.addEventListener('push', (e) => {
+  let options = {}
   self.registration.sendNotification(
-    e.waitUntil(self.registration.showNotification('push notifaction', {}))
+    e.waitUntil(self.registration.showNotification('push notifaction', options))
   )
 })
